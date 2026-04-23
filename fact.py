1574e734940bb9802cef93b6866d6406fd22b5c1
Himanshu Kumar <himanshu.it.24037@recb.ac.in>
2026-04-23 23:50:04 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,10 @@
+num = int (input("Enter number to find out the factorial "))
+
+fact  = 1 
+for i in range(1,num+1,1) : 
+    fact = fact*i
+
+print("the factorail of the ", num , " is  ", fact)
+print("thanks your")
+
+
